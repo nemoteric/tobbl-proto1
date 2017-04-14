d915684b3a7c6b6845120e98db30269ff2e8ee6f
sbhaaf <sbhaaf@dons.usfca.edu>
2017-04-14 13:31:09 -0700

    starting rebase

--- utils/commit.sh
+++ utils/commit.sh
@@ -5,6 +5,7 @@ find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 
 git add *
 git rm -rf --cached migrations
+git rm -rf --cached .idea
 git rm -rf --cached venv
 git rm -f --cached dev.db
 git commit -m "$1"
