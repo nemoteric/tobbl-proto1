f010a7a2d3a52c5b72fca0ffb87357ac8082b577
sbhaaf <sbhaaf@dons.usfca.edu>
2017-04-14 13:34:23 -0700

    starting rebase

--- utils/commit.sh
+++ utils/commit.sh
@@ -2,7 +2,7 @@
 
 ## remove all __pycache__ files
 find . -name '*.pyc' | xargs -n 1 git rm --cached
-find . -name '__pycache__' | xargs -n 1 git rm --cached
+find . -name '__pycache__' | xargs -n 1 git rm -rf --cached
 
 git add *
 git rm -rf --cached migrations
