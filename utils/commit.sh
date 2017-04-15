5e159f2d3f06b07f74d09025a3ecfc98d9348753
sbhaaf <sbhaaf@dons.usfca.edu>
2017-04-15 11:22:16 -0700

    delete pycache

--- utils/commit.sh
+++ utils/commit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## remove all __pycache__ files
-find . -name '*.pyc' | xargs -n 1 git rm --cached
-find . -name '__pycache__' | xargs -n 1 git rm -rf --cached
+find . -name '*.pyc' | xargs -n 1 git rm
+find . -name '__pycache__' | xargs -n 1 git rm -rf
 
 git add *
 git rm -rf --cached migrations
