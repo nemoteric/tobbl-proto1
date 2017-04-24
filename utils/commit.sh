42a77fa588d7cdf99b4c8b17be916f108c8db1ab
sbhaaf <sbhaaf@dons.usfca.edu>
2017-04-24 16:45:12 -0700

    prep for merge with master

--- utils/commit.sh
+++ utils/commit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## remove all __pycache__ files
+find . -name '*.pyc' | xargs -n 1 git rm
+find . -name '__pycache__' | xargs -n 1 git rm -rf
+
+rm -rf .idea/
+
 git add *
 git rm -rf --cached migrations
 git rm -rf --cached venv
